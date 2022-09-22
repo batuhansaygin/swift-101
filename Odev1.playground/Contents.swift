b7a586a9dc58db934e9ec4ac7b80ce841097c030
Batuhan Saygin <info@batuhansaygin.com>
2022-09-22 16:15:54 +0300

    Odev1 yuklendi

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var islemNo = 101
+var ogrNo = 145
+var kitapNo = 342
+var aTarih = "3 Eylül 2022"
+var vTarih = "23 Eylül 2022"
+
+var ogrAd = "Batuhan"
+var ogrSoyad = "Saygın"
+var cinsiyet = "Erkek"
+var dTarih = "5 Eylül 1996"
+var sinif = "iOS Developer"
+
+var isbnNo:Int = 452
+var kitapAdi = "iOS Geliştirici Rehberi"
+var yazarNo = 29
+var turNo = 223
+var sayfaSayisi = 427
+var puan:Float = 4.5
+
+var yazarAd = "Kasım"
+var yazarSoyad = "Adalan"
+
+var turAdi = "Eğitim"
